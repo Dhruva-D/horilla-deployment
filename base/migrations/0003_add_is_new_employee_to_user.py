24646e5a74caf4f0d517c2607f203ab8b2521ab8
Dhruva-D <dhruvad575@gmail.com>
2025-11-14 15:44:55 +0530

    Add migration for is_new_employee field on User model

---
+++ base/migrations/0003_add_is_new_employee_to_user.py
@@ -0,0 +1,19 @@
+# Generated migration to add is_new_employee field to auth.User
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('base', '0002_initial'),
+        ('auth', '0012_alter_user_first_name_max_length'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='user',
+            name='is_new_employee',
+            field=models.BooleanField(default=False),
+        ),
+    ]
