3b576a7c260a25078f18f1737f74681a87fbed57
Dhruva-D <dhruvad575@gmail.com>
2025-11-14 16:19:20 +0530

    Fix migration: use RunSQL for auth_user table

--- base/migrations/0003_add_is_new_employee_to_user.py
+++ base/migrations/0003_add_is_new_employee_to_user.py
@@ -11,9 +11,8 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.AddField(
-            model_name='user',
-            name='is_new_employee',
-            field=models.BooleanField(default=False),
+        migrations.RunSQL(
+            sql='ALTER TABLE auth_user ADD COLUMN is_new_employee BOOLEAN DEFAULT FALSE;',
+            reverse_sql='ALTER TABLE auth_user DROP COLUMN is_new_employee;',
         ),
     ]
